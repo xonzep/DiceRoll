37cd72286159604ab72545bdd0dfbe255ed82b93
Jeff McElhare <jmcelhare@gmail.com>
2022-11-15 22:15:33 -0500

    Add user input

--- Program.cs
+++ Program.cs
@@ -21,6 +21,12 @@ static void DiceRoll(int dCount, int dFaces)
 
     diceResults.ForEach(Console.WriteLine);
     var total = diceResults.Sum();
+    Console.WriteLine("The total amount is: " + total);
 }
 
-DiceRoll(8, 6);
+Console.WriteLine("How many dice do you need?");
+int userDiceNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("How many faces are on these dice?");
+int userDiceFace = Convert.ToInt32(Console.ReadLine());
+
+DiceRoll(userDiceNum, userDiceFace);
