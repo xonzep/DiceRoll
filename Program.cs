8d6122e43ca5902a8a0e03782e79b2ca93e70bbd
Jeff McElhare <jmcelhare@gmail.com>
2022-11-15 20:06:16 -0500

    Creates function that does a dice roll

--- Program.cs
+++ Program.cs
@@ -1,4 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 
+void DiceRoll(int dCount, int dFaces)
+{
+    List<int> diceResults = new();
 
+    for(int i = 1; i < dCount; i++)
+    {
+        int randomNumber =  new Random().Next(1, dFaces);
+        diceResults.Add(randomNumber);
+    }
+
+    diceResults.ForEach(Console.WriteLine);
+    var Total = diceResults.Sum();
+}
+
+DiceRoll(8, 6);
