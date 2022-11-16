2811bcb099815b7f57c2702c83b4f2cf065c809e
Jeff McElhare <jmcelhare@gmail.com>
2022-11-15 20:33:32 -0500

    Add problem breakdown comment

--- Program.cs
+++ Program.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 
-void DiceRoll(int dCount, int dFaces)
+/*
+ * Create a function that takes in the number of dice we're rolling and how many faces they have. 
+ * First we need a list to store our dice rolls.
+ * Then we need a random number between 1 and the number of faces our dice have.
+ * We can then call that for each dice and store that in our list.
+ * We then sum those numbers.
+*/
+static void DiceRoll(int dCount, int dFaces)
 {
     List<int> diceResults = new();
 
-    for(int i = 1; i < dCount; i++)
+    for (int i = 1; i < dCount; i++)
     {
         int randomNumber =  new Random().Next(1, dFaces);
         diceResults.Add(randomNumber);
